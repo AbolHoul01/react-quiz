8fc275e11940d9dbd0f7f70632e3610f3f2755a1
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-02-25 14:28:53 +0330

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="https://img.icons8.com/?size=100&id=asWSSTBrDlTW&format=png&color=000000"
+        alt="React Logo"
+      />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
